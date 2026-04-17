8d08a4431101153601f804ca23cd33df0bde2675
Nsanzimfura Innocent <nsanzimfurai580@gmail.com>
2026-04-17 11:22:56 +0200

    Update api.ts

--- api.ts
+++ api.ts
@@ -1,6 +1,7 @@
 import { Product, Order, StockMovement, Analytics, Expense, Income, LedgerSummary } from './types';
 
-const API_BASE = '/api';
+//const API_BASE = '/api';
+const API_BASE = import.meta.env.VITE_API_URL;
 
 async function handleResponse(res: Response) {
   if (!res.ok) {
