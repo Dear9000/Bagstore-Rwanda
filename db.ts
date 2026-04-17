b9ae3de22ad1c4ead60452f1da11295bd69b7a01
Nsanzimfura Innocent <nsanzimfurai580@gmail.com>
2026-04-17 12:22:41 +0200

    Update db.ts

--- db.ts
+++ db.ts
@@ -4,9 +4,9 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const dbPath = path.resolve(process.cwd(), 'database.sqlite');
-const db = new Database(dbPath);
+//const db = new Database(dbPath);
+const db = new Database(':memory:');
 
 // Initialize tables
 db.exec(`
